b0a35479cfe115546eea849121af8064d44bab93
jaelmaina20-hash <jaelmaina2.0@gmail.com>
2025-11-07 09:21:59 +0300

    Homepage and App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import TaskForm from "./components/TaskForm"
+import TaskList from "./components/TaskList"
+import Navbar from "./components/Navbar";
+
+
+
+
+function App() {
+ 
+
+
+  return (
+    <>
+     <Navbar/>
+     <TaskForm/>
+     <TaskList/>
+    </>
+  )
+}
+
+
+export default App
